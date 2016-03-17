5184b6386dd49a48f3bdfee01f673b487ded619c
Jonathan Marshall <jmarshall@never.you.mind>
2016-03-17 14:00:02 +1300

    adds MDS of some other stuff, modify current MDS a little

--- cluster_abundance.r
+++ cluster_abundance.r
@@ -101,15 +101,37 @@ y <- mds$points[,2]
 #z <- mds$points[,3]
 
 library(RColorBrewer)
-plot(x, y, main="MDS plot for MLST", type="n")
-text(x,y,labels=mlst2$ST, col=as.numeric(mlst2$Group))
+#plot(x, y, main="MDS plot for MLST", type="n")
+#text(x,y,labels=mlst2$ST, col=as.numeric(mlst2$Group))
 #pdf("mds.pdf", width=5, height=4)
 #ar(pin=c(5,4), omi=rep(0,4))
-pdf("mds.pdf", width=10, height=8)
+#pdf("mds.pdf", width=10, height=8)
 cols = c(brewer.pal(8, "Set2"), brewer.pal(8, "Dark2"), brewer.pal(8, "Accent"))
-plot(x, y, main="", xlim=c(-2,3), ylim=c(-2,2), type="n", xlab="", ylab="", xaxt="n", yaxt="n")
-points(x,y,col=cols[as.factor(samp.meta$animal)], pch = 16+as.numeric(as.factor(samp.meta$source)))
+plot(x, y, main="", type="n", xlab="", ylab="", xaxt="n", yaxt="n")
+points(x,y,col=cols[as.numeric(as.factor(samp.meta$animal))], pch = 16+as.numeric(as.factor(samp.meta$source)))
 legend("topright", legend=levels(as.factor(samp.meta$animal)), pch=19, col=cols, cex=0.8)
 legend("topleft", legend=levels(as.factor(samp.meta$source)), pch=16+1:4, col=cols[24], cex=0.8)
 #legend(2, 2, legend=c("chicken", "cow", "sheep", "water"), fill=1:4)
-dev.off()
+#dev.off()
+
+wch = samp.meta$animal == "ctrl"
+wch = wch | rowSums(abund_samp) < 200
+perm_diss_red = abund_dist[!wch, !wch]
+meta_red = samp.meta[!wch,]
+
+mds.red <- monoMDS(perm_diss_red,k=2)
+
+x <- mds.red$points[,1]
+y <- mds.red$points[,2]
+
+#pdf("mds.pdf", width=5, height=4)
+#ar(pin=c(5,4), omi=rep(0,4))
+#pdf("mds.pdf", width=10, height=8)
+cols = c(brewer.pal(8, "Set2"), brewer.pal(8, "Dark2"), brewer.pal(8, "Accent"))
+plot(x, y, main="", type="n", xlab="", ylab="", xaxt="n", yaxt="n")
+points(x,y,col=cols[as.factor(meta_red$animal)], pch = 16+as.numeric(as.factor(meta_red$source)))
+legend("topright", legend=levels(as.factor(meta_red$animal)), pch=19, col=cols, cex=0.8)
+legend("topleft", legend=levels(as.factor(meta_red$source)), pch=16+1:4, col=cols[24], cex=0.8)
+
+# TODO: BARPLOT FOR PATRICKS AND MINE, WITH HOPEFULLY NOT HORRID COLOURS!
+barplot(t(abund_samp/rowSums(abund_samp)))
